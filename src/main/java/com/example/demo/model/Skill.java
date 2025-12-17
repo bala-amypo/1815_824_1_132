8bfbcb83fe1586f0a75d2770924feef6c39e3aea
bala-amypo <bala@amypo.in>
2025-12-17 07:51:49 +0000

    User commit

--- src/main/java/com/example/demo/model/Skill.java
+++ src/main/java/com/example/demo/model/Skill.java
@@ -7,4 +7,32 @@ public class Skill{
     private String category;
     private String description;
     private Boolean active;
+
+    public long getId(){
+        return id;
+    }
+    public void setid(int id){
+        this.id=id;
+    }
+
+     public long getName(){
+        return name;
+    }
+    public void setid(int id){
+        this.id=id;
+    }
+
+     public long getId(){
+        return id;
+    }
+    public void setid(int id){
+        this.id=id;
+    }
+
+     public long getId(){
+        return id;
+    }
+    public void setid(int id){
+        this.id=id;
+    }
 }
