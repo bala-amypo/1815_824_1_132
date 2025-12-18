6d2aaefc5485cb37d815d7af1e42ba684483009f
bala-amypo <bala@amypo.in>
2025-12-18 10:36:36 +0000

    User commit

--- src/main/java/com/example/demo/service/SkillRequestService.java
+++ src/main/java/com/example/demo/service/SkillRequestService.java
@@ -1,1 +1,10 @@
-package com.example.demo.service;impo
+package com.example.demo.service;import;
+
+import jakarta.persistence.service;
+import com.example.demo.model.SkillRequest;
+@Service
+public interface SkillRequestService{
+    public SkillRequest createRequest(SkillRequest request);
+
+    public SkillRequest getSkillRequest(SkillRequest)
+}
