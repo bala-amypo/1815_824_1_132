e236479b780996c4b6860c2ce7e17112f283cf7d
bala-amypo <bala@amypo.in>
2025-12-18 08:26:35 +0000

    User commit

--- src/main/java/com/example/demo/model/SkillRequest.java
+++ src/main/java/com/example/demo/model/SkillRequest.java
@@ -1,6 +1,13 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import com.example.demo.model.UserProfile;
+import com.example.demo.model.Skill;
+
 
 @Entity
 public class SkillRequest {
