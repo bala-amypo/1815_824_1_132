1520a987612bcd3e3d8398e67a2d8b0a0c88d399
bala-amypo <bala@amypo.in>
2025-12-18 06:26:29 +0000

    User commit

--- src/main/java/com/example/demo/model/SkillOffer.java
+++ src/main/java/com/example/demo/model/SkillOffer.java
@@ -5,6 +5,31 @@ public class SkillOffer{
     @Id
     private Long id;
     private String user;
-    private 
-    
+    private String skill;
+    private String urgencyLevel;
+    private Boolean active;
+
+      public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+      public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getSkill() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
 }
