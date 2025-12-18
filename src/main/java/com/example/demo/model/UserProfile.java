6ff2a0760fb960ab5acaa121a48cc5c3ad8bf488
bala-amypo <bala@amypo.in>
2025-12-18 06:16:29 +0000

    User commit

--- src/main/java/com/example/demo/model/UserProfile.java
+++ src/main/java/com/example/demo/model/UserProfile.java
@@ -1,98 +1,98 @@
-// package com.example.demo.model;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Column;
-// import jakarta.persistence.PrePersist;
-// import org.hibernate.annotations.CreationTimestamp;
-// import org.hibernate.annotations.UpdateTimestamp;
-// import java.sql.Timestamp;
-// @Entity
-// public class UserProfile {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-//     private String bio;
-//     private Boolean active;
-//     @CreationTimestamp
-//     private Timestamp createdAt;
-//     @UpdateTimestamp
-//     private Timestamp updatedAt;
+package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.PrePersist;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.sql.Timestamp;
+@Entity
+public class UserProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String bio;
+    private Boolean active;
+    @CreationTimestamp
+    private Timestamp createdAt;
+    @UpdateTimestamp
+    private Timestamp updatedAt;
 
-//     @Column(unique = true)
-//     private String username;
-//     private String email;
+    @Column(unique = true)
+    private String username;
+    private String email;
 
-//     public Long getId() {
-//         return id;
-//     }
+    public Long getId() {
+        return id;
+    }
 
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-//     public String getUsername() {
-//         return username;
-//     }
+    public String getUsername() {
+        return username;
+    }
 
-//     public void setUsername(String username) {
-//         this.username = username;
-//     }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-//     public String getEmail() {
-//         return email;
-//     }
+    public String getEmail() {
+        return email;
+    }
 
-//     public void setEmail(String email) {
-//         this.email = email;
-//     }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-//     public String getBio() {
-//         return bio;
-//     }
+    public String getBio() {
+        return bio;
+    }
 
-//     public void setBio(String bio) {
-//         this.bio = bio;
-//     }
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
 
-//     public Boolean getActive() {
-//         return active;
-//     }
+    public Boolean getActive() {
+        return active;
+    }
 
-//     public void setActive(Boolean active) {
-//         this.active = active;
-//     }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 
  
-//     public Timestamp getCreatedAt() {
-//         return createdAt;
-//     }
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
 
-//     public void setCreatedAt(Timestamp createdAt) {
-//         this.createdAt = createdAt;
-//     }
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 
-//     public Timestamp getUpdatedAt() {
-//         return updatedAt;
-//     }
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
 
-//     public void setUpdatedAt(Timestamp updatedAt) {
-//         this.updatedAt = updatedAt;
-//     }
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 
-//     public UserProfile(Long id,String username,String email,String bio,Boolean active,Timestamp createdAt,Timestamp updatedAt){
-//     this.id=id;
-//     this.username=username;
-//     this.email=email;
-//     this.bio=bio;
-//     this.active=active;
-//     this.createdAt=createdAt;
-//     this.updatedAt=updatedAt;
+    public UserProfile(Long id,String username,String email,String bio,Boolean active,Timestamp createdAt,Timestamp updatedAt){
+    this.id=id;
+    this.username=username;
+    this.email=email;
+    this.bio=bio;
+    this.active=active;
+    this.createdAt=createdAt;
+    this.updatedAt=updatedAt;
 
-//     }
-//     public UserProfile(){
+    }
+    public UserProfile(){
 
-//     }
+    }
     
-// }
+}
