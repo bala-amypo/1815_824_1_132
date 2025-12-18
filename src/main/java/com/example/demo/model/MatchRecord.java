af12fc33f96271f53a77b20d8eb772a2138c9c4d
bala-amypo <bala@amypo.in>
2025-12-18 07:51:35 +0000

    User commit

--- src/main/java/com/example/demo/model/MatchRecord.java
+++ src/main/java/com/example/demo/model/MatchRecord.java
@@ -13,6 +13,18 @@ public class MatchRecord{
     private Timestamp matchedAt;
     private String status;
 
-    public Long getId()
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+
+        public Long getUserA(){
+        return userA;
+    }
+    public void setUserA(String ){
+        this.userA=userA;
+    }
 
 }
