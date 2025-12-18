fea14f06bcb2c0f84317f72b0cedae24fa848aaa
bala-amypo <bala@amypo.in>
2025-12-18 16:19:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/SkillOfferService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;import;
+
+import jakarta.persistence.service;
+import com.example.demo.model.SkillOffer;
+@Service
+public interface SkillOfferService{
+    public SkillOffer createRequest(SkillOffer request);
+
+    public SkillOffer getSkillOffer(SkillOffer)
+}
