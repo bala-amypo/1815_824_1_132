f2d8eb3345aa6c7289ee16f4142e15b108a4032b
bala-amypo <bala@amypo.in>
2025-12-18 08:21:35 +0000

    User commit

--- src/main/java/com/example/demo/model/SkillRequest.java
+++ src/main/java/com/example/demo/model/SkillRequest.java
@@ -1,7 +1,76 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+
 @Entity
-public SkillRequest {
+public class SkillRequest {
+
     @Id
-    @GeneratedValue(strategy=Generated)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private UserProfile user;
+
+    @ManyToOne
+    private Skill skill;
+
+    private String urgencyLevel; 
+
+    private Boolean active;
+
+    public SkillRequest() {
+    }
+
+    public SkillRequest(Boolean active, Long id, Skill skill, String urgencyLevel, UserProfile user) {
+        this.active = active;
+        this.id = id;
+        this.skill = skill;
+        this.urgencyLevel = urgencyLevel;
+        this.user = user;
+    }
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserProfile getUser() {
+        return user;
+    }
+
+    public void setUser(UserProfile user) {
+        this.user = user;
+    }
+
+    public Skill getSkill() {
+        return skill;
+    }
+
+    public void setSkill(Skill skill) {
+        this.skill = skill;
+    }
+
+    public String getUrgencyLevel() {
+        return urgencyLevel;
+    }
+
+    public void setUrgencyLevel(String urgencyLevel) {
+        this.urgencyLevel = urgencyLevel;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    
+
 }
