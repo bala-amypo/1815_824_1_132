e203812d0a8c294117bbff64dfa806c82b73f12c
bala-amypo <bala@amypo.in>
2025-12-18 04:51:29 +0000

    User commit

--- src/main/java/com/example/demo/model/UserProfile.java
+++ src/main/java/com/example/demo/model/UserProfile.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 // import jakarta.persistence.PrePersist;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.Update
+import org.hibernate.annotations.UpdateTimestamp;
 import java.sql.annotations;
 
 
