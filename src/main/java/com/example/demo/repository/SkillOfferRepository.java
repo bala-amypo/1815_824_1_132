1a9c60e7a0545e261548b84ab680044ba18e0f0e
bala-amypo <bala@amypo.in>
2025-12-18 10:11:36 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillOfferRepository.java
+++ src/main/java/com/example/demo/repository/SkillOfferRepository.java
@@ -3,6 +3,6 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.SkillOffer;
 
-public interface SkillOfferRepository extends JpaRepository<SkillOfferRepository,Long>{
+public interface SkillOfferRepository extends JpaRepository<SkillOffer,Long>{
 
 }
