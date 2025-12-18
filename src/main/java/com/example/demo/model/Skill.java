8fecd975d7857b837c2a0a47a8af797ae9ab7618
bala-amypo <bala@amypo.in>
2025-12-18 05:21:29 +0000

    User commit

--- src/main/java/com/example/demo/model/Skill.java
+++ src/main/java/com/example/demo/model/Skill.java
@@ -1,38 +1,38 @@
-// package com.example.demo.model;
+package com.example.demo.model;
 
-// @Entity
-// public class Skill{
-//     private Long id;
-//     private String name;
-//     private String category;
-//     private String description;
-//     private Boolean active;
+@Entity
+public class Skill{
+    private Long id;
+    private String name;
+    private String category;
+    private String description;
+    private Boolean active;
 
-//     public long getId(){
-//         return id;
-//     }
-//     public void setid(int id){
-//         this.id=id;
-//     }
+    public long getId(){
+        return id;
+    }
+    public void setid(int id){
+        this.id=id;
+    }
 
-//      public long getName(){
-//         return name;
-//     }
-//     public void setName(String name){
-//         this.name=name;
-//     }
+     public long getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
 
-//      public long getCategory(){
-//         return category;
-//     }
-//     public void setCategory(String category){
-//         this.category=category;
-//     }
+     public long getCategory(){
+        return category;
+    }
+    public void setCategory(String category){
+        this.category=category;
+    }
 
-//      public long getActive(){
-//         return active;
-//     }
-//     public void setActive(String active){
-//         this.active=active;
-//     }
-// }
+     public long getActive(){
+        return active;
+    }
+    public void setActive(String active){
+        this.active=active;
+    }
+}
