d062611f8276f02efb6565fc1a424c5e8ad2c28f
bala-amypo <bala@amypo.in>
2025-12-19 10:13:16 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Matchmakingimpl.java
+++ src/main/java/com/example/demo/service/impl/Matchmakingimpl.java
@@ -3,4 +3,6 @@ package com.example.demo.service.impl;
 import com.example.demo.repository.
 import java.util.List;
 import com.example.demo.service.MatchmakingService;
-import com.example.demo.repositoryMatchRecordRepository
+import com.example.demo.repositoryMatchRecordRepository;
+import org.springframework.sterotype.Service;
+import org.springframework.beans.factory.annotation
