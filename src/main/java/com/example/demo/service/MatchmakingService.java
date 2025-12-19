e4a3f3fdfd3b7d9cedd3f084e9b89fe4d3d3056f
bala-amypo <bala@amypo.in>
2025-12-19 04:00:36 +0000

    User commit

--- src/main/java/com/example/demo/service/MatchmakingService.java
+++ src/main/java/com/example/demo/service/MatchmakingService.java
@@ -1,10 +1,10 @@
 package com.example.demo.service;import;
 
 import jakarta.persistence.service;
-import com.example.demo.model.;
+import com.example.demo.model.MatchRecord;
 @Service
-public interface SkillRequestService{
-    public SkillRequest createRequest(SkillRequest request);
+public interface MatchmakingService{
+    public SkillRequest (SkillRequest request);
 
     public SkillRequest getSkillRequest(SkillRequest)
 }
