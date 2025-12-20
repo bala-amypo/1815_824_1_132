64568303cf822f809b3a8fe0c6f76e34a9d80e42
bala-amypo <bala@amypo.in>
2025-12-20 09:52:26 +0000

    User commit

--- src/main/java/com/example/demo/service/MatchmakingService.java
+++ src/main/java/com/example/demo/service/MatchmakingService.java
@@ -1,9 +1,11 @@
 package com.example.demo.service;import;
 
 import jakarta.persistence.service;
+import jakarta.persistence.*;
 import com.example.demo.model.MatchRecord;
 @Service
 public interface MatchmakingService{
+@Id
     public MatchRecord generateMatch(Long userId);
     public MatchRecord getMatchById(Long id);
     public MatchRecord getMatchesForUser(Long userId);
