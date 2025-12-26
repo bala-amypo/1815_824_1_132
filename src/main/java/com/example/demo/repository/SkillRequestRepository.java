6a9e9f4d13667ff384ce5927426303830d3aa36e
bala-amypo <bala@amypo.in>
2025-12-26 10:38:58 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillRequestRepository.java
+++ src/main/java/com/example/demo/repository/SkillRequestRepository.java
@@ -2,10 +2,16 @@ package com.example.demo.repository;
 
 import com.example.demo.model.SkillRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface SkillRequestRepository extends JpaRepository<SkillRequest, Long> {
 
-    // Use entity field name "skill" and navigate to "id"
+    // Find requests by Skill ID
     List<SkillRequest> findBySkill_Id(Long skillId);
+
+    // Find requests by User ID
+    List<SkillRequest> findByUser_Id(Long userId);
 }
