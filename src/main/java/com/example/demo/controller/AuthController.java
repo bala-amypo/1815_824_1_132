28d9b06d7eafc3c6dcc3124025d9bf1e9a3b6a8f
bala-amypo <bala@amypo.in>
2025-12-26 09:05:55 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,30 +1,19 @@
-// package com.example.demo.controller;
+package com.example.demo.config;
 
-// import com.example.demo.model.User;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// import com.example.demo.service.UserService;
-// import org.springframework.web.bind.annotation.*;
+@Configuration
+public class OpenApiConfig {
 
-// @RestController
-// @RequestMapping("/auth")
-// @io.swagger.v3.oas.annotations.tags.Tag(name = "Authentication")
-// public class AuthController {
-
-//     private final UserService userService;
-
-
-//     public AuthController(UserService userService) {
-//         this.userService = userService;
-//     }
-
-//     @PostMapping("/register")
-//     public User register(@RequestBody User user) {
-//         return userService.register(user);
-//     }
-
-//     @PostMapping("/login")
-//     public String login(@RequestBody User user) {
-//         User u = userService.findByEmail(user.getEmail());
-//         return null;
-//     }
-// }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Skill Barter Matchmaking API")
+                        .version("1.0")
+                        .description("JWT-secured Skill Barter Platform"));
+    }
+}
