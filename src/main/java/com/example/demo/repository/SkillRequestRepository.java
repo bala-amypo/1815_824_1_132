07fa44198415301d4d7b8daa4f075123691e3d69
bala-amypo <bala@amypo.in>
2025-12-26 07:50:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/SkillRequestRepository.java
+++ src/main/java/com/example/demo/repository/SkillRequestRepository.java
@@ -1,10 +1,11 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.SkillRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.example.demo.model.SkillRequest;
 import java.util.List;
 
-public interface SkillRequestRepository extends JpaRepository<SkillRequest, Long> {
+public interface SkillRequestRepository
+        extends JpaRepository<SkillRequest, Long> {
+
     List<SkillRequest> findByUserId(Long userId);
 }
--- src/main/java/com/example/demo/service/MatchmakingService.java
+++ src/main/java/com/example/demo/service/MatchmakingService.java
@@ -1,15 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.model.MatchRecord;
-import java.util.List;
 
 public interface MatchmakingService {
 
     MatchRecord generateMatch(Long userId);
-
     MatchRecord getMatchById(Long id);
-
-    List<MatchRecord> getMatchesForUser(Long userId);
-
+    MatchRecord getMatchesForUser(Long userId);
     MatchRecord updateMatchStatus(Long id, String status);
 }
