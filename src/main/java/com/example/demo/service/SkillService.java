c6e7f726c4f256b5d408e934bfed5ba80c02f0c4
bala-amypo <bala@amypo.in>
2025-12-26 18:14:05 +0000

    User commit

--- src/main/java/com/example/demo/service/SkillService.java
+++ src/main/java/com/example/demo/service/SkillService.java
@@ -4,8 +4,14 @@ import com.example.demo.model.Skill;
 import java.util.List;
 
 public interface SkillService {
+
     Skill createSkill(Skill skill);
+
     Skill updateSkill(Long id, Skill skill);
+
     Skill getSkillById(Long id);
-    List<Skill
+
+    List<Skill> getAllSkills();
+
+    void deactivateSkill(Long id);
 }
