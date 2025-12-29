440110ec7ea721429ff1800f2bd1d8cec43a6b0c
bala-amypo <bala@amypo.in>
2025-12-29 09:09:25 +0000

    User commit

--- src/main/java/com/example/demo/model/UserProfile.java
+++ src/main/java/com/example/demo/model/UserProfile.java
@@ -10,6 +10,9 @@ public class UserProfile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true, nullable = false)
+
+    
 
     private String username;
     private String email;
